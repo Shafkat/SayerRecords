4afd920603f3ac4aa173ca92fab5811b296ce210
Shafkat <shafkatulhaque@live.ca>
2011-10-20 08:42:07 -0400

    Playing around.

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,1 +1,8 @@
 //This file contains the main functionality of the site.
+
+$(document).ready(function() {
+	$("#sayers").hover(function () {
+		$('#logo').fadeOut(2500);
+		//$('#tabs').tabs();
+	});
+});
