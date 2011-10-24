6af685ddae32a652bd79125cccd88cb8aa39f0ab
Shafkat <shafkatulhaque@live.ca>
2011-10-24 17:50:16 -0400

    Added music player. Fade in transitions. Advanced css effects. This is awesome.

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,8 +1,16 @@
 //This file contains the main functionality of the site.
 
 $(document).ready(function() {
-	$("#sayers").hover(function () {
-		$('#logo').fadeOut(2500);
+	$('#player').hide();
+	$("#records").hover(function () {
+		$('#records_image').fadeOut(3000);
+		$('#records_image').hide(2500, function() { $("#player").fadeIn(2500);});
 		//$('#tabs').tabs();
+	});
+	
+	$('#page-wrap').hide();
+	$("#sayers_image").hover(function () {
+	$('#sayers_image').fadeOut(2500);
+	$('#sayers_image').hide(2500, function() { $("#page-wrap").fadeIn(3000);});
 	});
 });
---
+++ js/myplaylist.js
@@ -0,0 +1,33 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'music/shesh_kobita.mp3',
+        oga:'music/shesh_kobita.ogg',
+        title:'Shesh Kobita',
+        artist:'SoothSayer',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:52',
+        cover:'images/more_images/Album Cover.jpg'
+    },
+    {
+        mp3:'music/alor_shokal.mp3',
+        oga:'music/alor_shokal.ogg',
+        title:'Alor Shokal',
+        artist:'SoothSayer',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:45',
+        cover:'images/more_images/Album Cover.jpg'
+    }
+];
