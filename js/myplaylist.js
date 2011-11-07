2de17dfeb63e9ddad6f2d68d815860da23ec82a5
Shafkat <Shafkatulhaque@live.ca>
2011-11-06 22:48:34 -0500

    Refactoring...

--- js/myplaylist.js
+++ js/myplaylist.js
@@ -13,6 +13,17 @@ var myPlaylist = [
         cover:'images/more_images/Album Cover.jpg'
     },
     {
+        mp3:'music/Hotta_Kore_Amaye.mp3',
+        oga:'music/Hotta_Kore_Amaye.ogg',
+        title:'Hotta Kore Amaye',
+        artist:'SoothSayer',
+        rating:5,
+        //buy:'#',
+        //price:'0.99',
+        duration:'3:34',
+        cover:'images/more_images/Album Cover.jpg'
+    },
+    {
         mp3:'music/alor_shokal.mp3',
         oga:'music/alor_shokal.ogg',
         title:'Alor Shokal',
